5d5a8b62a2f12b5e399e123be4bc18e56c0fc936
diogenesleandro <diogenes.pereira@cps.sp.gov.br>
2026-05-27 11:41:18 -0300

    aula 27 05

--- consulta.php
+++ consulta.php
@@ -1,13 +1,15 @@
 <?php
 function consultar(){
-    $resposta="<table border='1'><tr><th>Nome</th><th>Distrito</th><th>Populaao</th><th>Excluir</th></tr>";
+    $resposta="<table border='1' class='table table-bordered table-striped table-responsive'><tr><th>Nome</th><th>Distrito</th><th>População</th><th>Ação</th></tr>";
 include 'conecta.php';
-$stmt = $pdo->query('SELECT * from city');
+$stmt = $pdo->query('SELECT * from city ORDER BY ID DESC');
 while ($row = $stmt->fetch()) {
     $resposta.="<tr><td>".$row['Name'].
     "</td><td>".$row['District'].
     "</td><td>".$row['Population'].
-    "</td><td><button class='excluir' id=".$row['ID'].">excluir</button></td></tr>";
+    "</td><td>".
+    "<button class='excluir btn btn-danger btn-lg' id=".$row['ID'].">excluir</button>".
+    "<button class='editar btn btn-warning btn-lg' id=".$row['ID'].">editar</button></td></tr>";
 }
 $resposta.="</table>";
 echo $resposta;
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php
+  <title>Cadastro de Cidade</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <?php
     include 'consulta.php';
     ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
-$(function() {
+    $(function() {
     // Evento de Gravação (Inserir)
     $("#gravar").on("click", function() {
         var nome = $('#name').val();
@@ -26,9 +28,9 @@ $(function() {
             },
             dataType: "html"
         }).done(function(resposta) {
-            $("div").html(resposta);
+            $("#resultado").html(resposta);
         }).fail(function(jqXHR, textStatus) {
-            $("div").html("Request failed: " + textStatus);
+            $("#resultado").html("Request failed: " + textStatus);
         }).always(function() {
             console.log("completou gravação");
         });
@@ -39,35 +41,71 @@ $(function() {
 // Evento de Exclusão Corrigido com Delegação de Eventos
 $(document).on("click", ".excluir", function() {
     var id = $(this).attr("id");
-    
+   if(confirm("Deseja excluir essa cidade realmente")){
     $.ajax({
         url: "apaga.php",
         type: "POST",
         data: { id: id },
         dataType: "html"
     }).done(function(resposta) {
-        $("div").html(resposta);
+        $("#resultado").html(resposta);
     }).fail(function(jqXHR, textStatus) {
-        $("div").html("Request failed: " + textStatus);
+        $("#resultado").html("Request failed: " + textStatus);
     }).always(function() {
         console.log("completou exclusão");
     });
+
+   }
+
 });
 });
 </script>
-    <title>Document</title>
 </head>
-<body>
-    <input type="text" id="name">
-    <input type="text" id="district">
-    <input type="text" id="population">
-    <button id="gravar"> Gravar</button>
-    <div>
+ <body>
+    <div class="container">
+         <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" style="margin:10px;">Cadastrar Cidade</button>
+         <!-- Modal -->
+          <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <div class="form-group">   
+      <label for="usr">Nome:</label>
+      <input type="text" class="form-control" id="name">
+    </div>
+    <div class="form-group">
+      <label for="usr">Distrito:</label>
+      <input type="text" class="form-control" id="district">
+    </div>
+    <div class="form-group">
+      <label for="usr">População:</label>
+      <input type="text" class="form-control" id="population">
+    </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal" id="gravar">Gravar</button>
+        </div>
+      </div>
+      
+    </div>
+</div>
+
+
+
+    <div id="resultado" class="p-5">
 
     <?php
         consultar();
     ?>
 
     </div>
+</div>
+
 </body>
 </html>
